d6f665f3042a27bee2ca8e28b0e4f231ed6847a6
jkjnmlk <123@example.com>
2019-01-17 15:34:23 +0800

    构建基本结构框架

--- main.c
+++ main.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("欢迎进专车管理系统\n");
+
+    while (1)
+    {
+        printf("1--添加车辆位置\n");
+        printf("2--删除最后一个车辆位置\n");
+        printf("3--显示所有车辆位置\n");
+        printf("4--返回离我最近的车辆\n");
+        printf("5--退出\n");
+        printf("请选择所需功能\n");
+        int code;
+        scanf("%d",&code);
+        
+        if (code==1) {
+            /* code */
+        }
+        if (code==2) {
+            /* code */
+        }
+        if (code==3) {
+            /* code */
+        }
+        if (code==4) {
+            /* code */
+        }
+        if (code==5) {
+            printf("点击回车，退出系统\n");
+            break;
+        }
+        
+    }
 
     return 0;
-
 }
